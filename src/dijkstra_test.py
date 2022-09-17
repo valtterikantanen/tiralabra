a23012d0b31792715e6f1ca4353433655e8553f4
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-09-17 23:58:34 +0300

    Fix Pylint errors

--- src/dijkstra_test.py
+++ src/dijkstra_test.py
@@ -3,7 +3,7 @@ from graph import make_adjacency_lists
 
 def collect_known_distances(filename):
     with open(filename) as file:
-        known_distances = {}
+        distances = {}
 
         for row, line in enumerate(file):
             if row == 0:
@@ -11,13 +11,13 @@ def collect_known_distances(filename):
             line = line.split()
             start = int(line[5]) * int(line[2]) + int(line[4])
             end = int(line[7]) * int(line[2]) + int(line[6])
-            known_distances[(start, end)] = float(line[8])
-    return known_distances
+            distances[(start, end)] = float(line[8])
+    return distances
 
-def write_test_results(graph, known_distances, filename):
+def write_test_results(graph, correct_distances, filename):
     with open(filename, "w") as file:
         file.write("start_node;end_node;calculated_distance;known_distance;difference\n")
-        for key, value in known_distances.items():
+        for key, value in correct_distances.items():
             start = key[0]
             end = key[1]
             distance = dijkstra(graph, start, end)
@@ -25,6 +25,6 @@ def write_test_results(graph, known_distances, filename):
             file.write(f"{start};{end};{distance};{value};{difference}\n")
 
 if __name__ == "__main__":
-    graph = make_adjacency_lists("maps/Berlin_0_256.map")
+    adjacency_lists = make_adjacency_lists("maps/Berlin_0_256.map")
     known_distances = collect_known_distances("maps/Berlin_0_256.map.scen")
-    write_test_results(graph, known_distances, "result.csv")
+    write_test_results(adjacency_lists, known_distances, "result.csv")
--- src/graph.py
+++ src/graph.py
@@ -1,18 +1,18 @@
 from math import sqrt
 
-def make_adjacency_lists(filename: str):
+def make_adjacency_lists(filename):
     with open(filename) as file:
-        map = []
+        map_rows = []
 
         for i, line in enumerate(file):
             # Ensimmäiset neljä riviä sisältävät tietoa kartasta
             if i >= 4:
                 line = line.replace("\n", "")
-                row = [i for i in line]
-                map.append(row)
+                row = list(line)
+                map_rows.append(row)
 
-    height = len(map)
-    width = len(map[0])
+    height = len(map_rows)
+    width = len(map_rows[0])
 
     # Esitetään verkko vieruslistoina ja alustetaan jokainen lista tyhjäksi
     graph = [[] for _ in range(height * width)]
@@ -20,7 +20,7 @@ def make_adjacency_lists(filename: str):
     empty = "."
     occupied = "@"
 
-    for i, row in enumerate(map): # rivin numero = y-koordinaatti (0–255)
+    for i, row in enumerate(map_rows): # rivin numero = y-koordinaatti (0–255)
         for j, column in enumerate(row): # sarakkeen numero = x-koordinaatti (0–255)
             if column == occupied:
                 continue
@@ -30,24 +30,24 @@ def make_adjacency_lists(filename: str):
                 y_coord = i + option[0]
                 x_coord = j + option[1]
                 # Tarkistetaan, menevätkö koordinaatit reunojen yli
-                if not (0 <= x_coord < width) or not (0 <= y_coord < height):
+                if not 0 <= x_coord < width or not 0 <= y_coord < height:
                     continue
                 # Viistoon voidaan liikkua vain, jos kaksi sen viereistä ruutua ovat vapaana
                 if option[0] != 0 and option[1] != 0:
                     if option == (-1, -1):
-                        if map[i-1][j] != empty or map[i][j-1] != empty:
+                        if map_rows[i-1][j] != empty or map_rows[i][j-1] != empty:
                             continue
                     elif option == (-1, 1):
-                        if map[i-1][j] != empty or map[i][j+1] != empty:
+                        if map_rows[i-1][j] != empty or map_rows[i][j+1] != empty:
                             continue
                     elif option == (1, -1):
-                        if map[i][j-1] != empty or map[i+1][j] != empty:
+                        if map_rows[i][j-1] != empty or map_rows[i+1][j] != empty:
                             continue
                     elif option == (1, 1):
-                        if map[i][j+1] != empty or map[i+1][j] != empty:
+                        if map_rows[i][j+1] != empty or map_rows[i+1][j] != empty:
                             continue
-                if map[y_coord][x_coord] == empty:
-                    # Jos liikuttiin vain vaaka- tai pystysuunnassa, kaaren paino on 1, muuten sqrt(2)
+                if map_rows[y_coord][x_coord] == empty:
+                    # Jos liikuttiin vaaka- tai pystysuunnassa, kaaren paino on 1, muuten sqrt(2)
                     weight = 1 if option[0] == 0 or option[1] == 0 else sqrt(2)
                     endpoint = y_coord * width + x_coord
                     graph[node].append((weight, endpoint))
